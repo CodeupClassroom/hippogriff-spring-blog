9a12eeba73d649e21b22f52f08f1e4d2c7266148
madmarcos-codeup <mark.robinson@codeup.com>
2023-12-08 10:38:08 -0600

    first commit

---
+++ src/main/java/com/codeup/hippogriffspringblog/HippogriffSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.hippogriffspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippogriffSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HippogriffSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/hippogriffspringblog/HippogriffSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.hippogriffspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippogriffSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
