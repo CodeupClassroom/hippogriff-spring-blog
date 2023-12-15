1329b2b2cba5fe79afd9da35721a9ebdb16560d3
madmarcos-codeup <mark.robinson@codeup.com>
2023-12-15 14:27:47 -0600

    post fmb walkthru

--- src/main/java/com/codeup/hippogriffspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/hippogriffspringblog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.codeup.hippogriffspringblog.controllers;
 
 import com.codeup.hippogriffspringblog.dao.PostRepository;
 import com.codeup.hippogriffspringblog.dao.UserRepository;
+import com.codeup.hippogriffspringblog.models.Ad;
 import com.codeup.hippogriffspringblog.models.Post;
 import com.codeup.hippogriffspringblog.models.User;
 import org.springframework.stereotype.Controller;
@@ -11,18 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
-    private Post post1 = new Post(1, "Post 1", "qewrqwerqwerqwe");
-    private Post post2 = new Post(2, "Post 2", "qewrqwerqwerqwe");
-    private Post post3 = new Post(3, "Post 3", "qewrqwerqwerqwe");
-
-    // Arrays.asList(1, 2, 3)
-    private List<Post> posts = new ArrayList<>(List.of(post1, post2, post3));
-
-
     private final PostRepository postDao;
     private final UserRepository userDao;
 
@@ -54,18 +48,53 @@ public class PostController {
     }
 
     @GetMapping("/create")
-    public String getCreatePage() {
+    public String getCreatePage(Model model) {
+        model.addAttribute("post", new Post());
         return "/posts/create";
     }
 
     @PostMapping("/create")
-    public String submitPost(@RequestParam(name = "title") String title,
-                             @RequestParam(name = "body") String body) {
-        Post post = new Post(title, body);
+    public String submitPost(@ModelAttribute Post post) {
         User user = userDao.findUserById(1L);
         post.setUser(user);
         postDao.save(post);
 
         return "redirect:/posts";
     }
+
+    @GetMapping("/{id}/edit")
+    public String showEditForm(@PathVariable long id,
+                               Model model) {
+        Post post;
+        if (postDao.findById(id).isPresent()) {
+            post = postDao.findById(id).get();
+        } else {
+            post = null;
+        }
+        model.addAttribute("post", post);
+        return "/posts/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editAd(@ModelAttribute Post modifiedPost){
+        // validate the incoming posts changes?
+        if(modifiedPost.getTitle().trim().isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be blank!");
+        }
+
+        // go grab the post's user from the already saved post in the db
+        // and set that user's id in the modified post
+        Post oldPost = null;
+        Optional<Post> checkPost = postDao.findById(modifiedPost.getId());
+        if(checkPost.isPresent()) {
+            oldPost = checkPost.get();
+        }
+        if(oldPost != null) {
+            modifiedPost.setUser(oldPost.getUser());
+        }
+
+        postDao.save(modifiedPost);
+        return "redirect:/posts";
+    }
+
 }
